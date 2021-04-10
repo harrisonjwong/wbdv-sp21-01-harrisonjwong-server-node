bd180c60b5fffd9770c5a78ff586fb8aa5f02408
Harrison Wong <harrisonjwong@yahoo.com>
2021-04-10 15:58:33 -0400

    Update question-controller

--- controllers/question-controller.js
+++ controllers/question-controller.js
@@ -3,8 +3,8 @@ const questionsService = require('../services/questions-service');
 module.exports = (app) => {
   const findQuestionsForQuiz = (req, res) => {
     const quizId = req.params['qid'];
-    const questions = questionsService.findQuestionsForQuiz(quizId);
-    res.json(questions);
+    questionsService.findQuestionsForQuiz(quizId)
+      .then(questions => res.json(questions));
   }
 
   app.get('/api/quizzes/:qid/questions', findQuestionsForQuiz);
