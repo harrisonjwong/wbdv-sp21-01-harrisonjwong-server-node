71e557605aea529f6b7db9375e3bd6c8c1750812
Harrison Wong <harrisonjwong@yahoo.com>
2021-04-10 15:58:54 -0400

    Create quizzes-schema

---
+++ models/quizzes/quizzes-schema.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const quizzesSchema = mongoose.Schema({
+  _id: String,
+  title: String,
+  questions: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'QuestionsModel'
+  }]
+}, {collection: 'quizzes'});
+
+module.exports = quizzesSchema;
